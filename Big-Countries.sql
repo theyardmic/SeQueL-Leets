34086986046a24c75b217ca27b901c9f3d4c5bdd
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-11-08 01:13:54 -0800

    Update Big-Countries.sql

--- Big-Countries.sql
+++ Big-Countries.sql
@@ -5,7 +5,7 @@ it has a population of at least twenty-five million (i.e., 25000000).
 Write an SQL query to report the name, population, and area of the big countries.
 */
 
-/* Input: 
+// Input: 
 World table:
 +-------------+-----------+---------+------------+--------------+
 | name        | continent | area    | population | gdp          |
@@ -17,17 +17,14 @@ World table:
 | Angola      | Africa    | 1246700 | 20609294   | 100990000000 |
 +-------------+-----------+---------+------------+--------------+
 
-*/
 
 SELECT name, population, area FROM World WHERE area >= 3000000 || population >= 25000000;
 
-/*
-Output: 
+//Output: 
 +-------------+------------+---------+
 | name        | population | area    |
 +-------------+------------+---------+
 | Afghanistan | 25500100   | 652230  |
 | Algeria     | 37100000   | 2381741 |
 +-------------+------------+---------+
-*/
 
