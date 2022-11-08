47acf86853125898d9b55c595ae9b010911a05df
Strucker.eth <93291951+strucker-eth@users.noreply.github.com>
2022-11-08 01:51:42 -0800

    Create WHERE + NULL.sql

---
+++ WHERE + NULL.sql
@@ -0,0 +1,26 @@
+/*Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+*/
+
+/* Write an SQL query to report the names of the customer that are not referred by the customer with id = 2. */
+
+SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS NULL;
+
+--Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
