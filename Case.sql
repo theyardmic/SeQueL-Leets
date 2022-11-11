f5f71cac1c5334f55555063354baeed69517bf27
Strucker.eth <aberamacarleton@gmail.com>
2022-11-11 21:26:23 +0300

    Case SQL application

---
+++ Case.sql
@@ -0,0 +1,37 @@
+/* Write an SQL query to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id. 
+
+Employees table:
++-------------+---------+--------+
+| employee_id | name    | salary |
++-------------+---------+--------+
+| 2           | Meir    | 3000   |
+| 3           | Michael | 3800   |
+| 7           | Addilyn | 7400   |
+| 8           | Juan    | 6100   |
+| 9           | Kannon  | 7700   |
++-------------+---------+--------+
+
+*/
+
+  select employee_id , 
+   case when (employee_id%2) <> 0 and name not like 'M%' then salary 
+   else 0
+   end 
+   as bonus
+   from Employees
+   order by employee_id;
+
+   /*
+Output: 
++-------------+-------+
+| employee_id | bonus |
++-------------+-------+
+| 2           | 0     |
+| 3           | 0     |
+| 7           | 7400  |
+| 8           | 0     |
+| 9           | 7700  |
++-------------+-------+
+*/   
