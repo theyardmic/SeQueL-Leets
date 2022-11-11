d0faaf0ae1b65d45ce777a880269018eb42f9a6d
Strucker.eth <aberamacarleton@gmail.com>
2022-11-11 21:50:47 +0300

    Applying  Cartesian Product for data validation

---
+++ Cartesian product.sql
@@ -0,0 +1,27 @@
+/*
+Write an SQL query to delete all the duplicate emails, keeping only one unique email with the smallest id. Note that you are supposed to write a DELETE statement and not a SELECT one.
+
+After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.
+
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+*/
+
+delete p1 from person p1,person p2 
+where p1.email = p2.email and p1.id > p2.id;
+
+/* Output:
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+*/
