d59a440aa09acffd8e67fe2be8e606f45b9bb523
Strucker.eth <aberamacarleton@gmail.com>
2022-11-14 22:22:46 +0300

    comments

--- WHERE + NULL.sql
+++ WHERE + NULL.sql
@@ -14,7 +14,7 @@
 /* Write an SQL query to report the names of the customer that are not referred by the customer with id = 2. */
 
 SELECT name FROM Customer WHERE referee_id != 2 OR referee_id IS NULL;
-
+/*get name from customer table were reference id is Null*/
 --Output: 
 +------+
 | name |
